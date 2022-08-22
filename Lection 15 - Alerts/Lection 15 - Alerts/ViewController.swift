c7319b44c28452ba7482ce04576617e6afaff43d
Denis Kutsko <>
2022-08-22 09:57:45 +0300

    added project

---
+++ Lection 15 - Alerts/Lection 15 - Alerts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lection 15 - Alerts
+//
+//  Created by Denis Kutsko on 22/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
